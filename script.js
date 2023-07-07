5be87b4d34883bbe62f541c7de6ee60f8b5c8618
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-function threeSum(decimal) {
-if (decimal === 0) {
-    return '0'; // Special case for input 0
-  }
-
-  let binary = '';
-  
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
-  
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-module.exports = threeSum;
+window.decimalToBinary = decimalToBinary;
